8b41b3855c783b0df190818dc417880fecd8575c
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-27 07:04:36 +0530

    Completed SumOfFirstNOddAndEvenNumbers

---
+++ com/SixPhrase/Day_1/SumOfFirstNOddAndEvenNumbers.java
@@ -0,0 +1,12 @@
+package com.SixPhrase.Day_1;
+
+public class SumOfFirstNOddAndEvenNumbers {
+    public static void main(String[] args) {
+        int n = 7;
+        // sum of first n odd numbers  = n^2
+        System.out.println(n*n);
+        // sum of first n even numbers = n*(n+1)
+        System.out.println(n*(n+1));
+
+    }
+}
