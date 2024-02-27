c771d90a43ae090d987b1f4b67bd396041446001
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-27 07:01:45 +0530

    Completed NumberAsSumOfTwoPrimes

--- com/SixPhrase/Day_1/NumberAsSumOfTwoPrimes.java
+++ com/SixPhrase/Day_1/NumberAsSumOfTwoPrimes.java
@@ -1,4 +1,52 @@
 package com.SixPhrase.Day_1;
 
+import java.util.Scanner;
+
 public class NumberAsSumOfTwoPrimes {
+    public static void main(String[] args) {
+        canExpressAsSumOf2Primes(10);
+        canExpressAsSumOf2Primes(34);
+        canExpressAsSumOf2Primes(43);
+        canExpressAsSumOf2Primes(37);
+        canExpressAsSumOf2Primes(29);
+        canExpressAsSumOf2Primes(11);
+    }
+
+    public static void canExpressAsSumOf2Primes(int n) {
+        if (n % 2 == 0) {
+            System.out.println("yes");
+            ;
+        } else if (n % 2 != 0 && isPrime(n - 2)) {
+            System.out.println("yes");
+        } else {
+            System.out.println("no");
+        }
+    }
+
+    public static boolean isPrime(int n) {
+        if (n == 2 || n == 3) {
+            return true;
+        } else if (n % 2 == 0) {
+            return false;
+        } else {
+            return ((n / 2) - 1) % 3 != 0;
+        }
+    }
 }
+//    public static boolean isPrime(int n){
+//        if (n <= 1)
+//            return false;
+//
+//        if (n == 2 || n == 3)
+//            return true;
+//
+//        if (n % 2 == 0 || n % 3 == 0)
+//            return false;
+//
+//        for (int i = 5; i < Math.sqrt(n); i = i + 6)
+//            if (n % i == 0 || n % (i + 2) == 0)
+//                return false;
+//
+//        return true;
+//    }
+
