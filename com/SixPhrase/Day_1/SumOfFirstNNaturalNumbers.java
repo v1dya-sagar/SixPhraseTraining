8a63a42f64cc023eb4d6185dba188c54b75cdf7c
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-27 07:05:52 +0530

    Completed SumOfFirstNNaturalNumbers

---
+++ com/SixPhrase/Day_1/SumOfFirstNNaturalNumbers.java
@@ -0,0 +1,10 @@
+package com.SixPhrase.Day_1;
+
+public class SumOfFirstNNaturalNumbers {
+    public static void main(String[] args) {
+        int n = 11;
+        // n*(n+1)/2
+        System.out.println(n*(n+1)/2);
+
+    }
+}
