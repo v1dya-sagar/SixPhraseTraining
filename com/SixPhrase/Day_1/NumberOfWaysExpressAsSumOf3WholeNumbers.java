8978b1ecb42b5a4190702cd6179663939d1ae333
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-27 07:08:02 +0530

    Completed NumberOfWaysExpressAsSumOf3WholeNumbers

---
+++ com/SixPhrase/Day_1/NumberOfWaysExpressAsSumOf3WholeNumbers.java
@@ -0,0 +1,10 @@
+package com.SixPhrase.Day_1;
+
+public class NumberOfWaysExpressAsSumOf3WholeNumbers {
+    public static void main(String[] args) {
+        int x = 3;
+        int n = x + 1;
+        int result = n*(n+1)/2;
+        System.out.println(result);
+    }
+}
