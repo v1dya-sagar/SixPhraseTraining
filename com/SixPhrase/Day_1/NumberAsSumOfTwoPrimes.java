0939166895a84a3f90aee01fb498ed159517866e
v1dya-sagar <vidyasagar08123@gmail.com>
2024-02-28 05:36:34 +0530

    Commit for changes

--- com/SixPhrase/Day_1/NumberAsSumOfTwoPrimes.java
+++ com/SixPhrase/Day_1/NumberAsSumOfTwoPrimes.java
@@ -22,31 +22,34 @@ public class NumberAsSumOfTwoPrimes {
             System.out.println("no");
         }
     }
-
-    public static boolean isPrime(int n) {
-        if (n == 2 || n == 3) {
-            return true;
-        } else if (n % 2 == 0) {
+    public static boolean isPrime(int n){
+        if (n <= 1)
             return false;
-        } else {
-            return ((n / 2) - 1) % 3 != 0;
-        }
+
+        if (n == 2 || n == 3)
+            return true;
+
+        if (n % 2 == 0 || n % 3 == 0)
+            return false;
+
+        for (int i = 5; i < Math.sqrt(n); i = i + 6)
+            if (n % i == 0 || n % (i + 2) == 0)
+                return false;
+
+        return true;
     }
-}
-//    public static boolean isPrime(int n){
-//        if (n <= 1)
-//            return false;
-//
-//        if (n == 2 || n == 3)
-//            return true;
-//
-//        if (n % 2 == 0 || n % 3 == 0)
-//            return false;
-//
-//        for (int i = 5; i < Math.sqrt(n); i = i + 6)
-//            if (n % i == 0 || n % (i + 2) == 0)
-//                return false;
-//
+//    isPrime(num)
+//        if(num == 2 ){
 //        return true;
 //    }
+//    int val = (int) Math.sqrt(num);
+//        for(int i = 3;i<=val;i+=2){
+//        if(num % i ==0 ){
+//            return false;
+//        }
+//    }
+//        return true;
+//}
+}
+
 
