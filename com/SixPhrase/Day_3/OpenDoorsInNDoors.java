281b7d31ddace25e1d2c345a07204f9575c53480
v1dya-sagar <vidyasagar08123@gmail.com>
2024-03-01 18:08:52 +0530

    Completed OpenDoorsInNDoors

---
+++ com/SixPhrase/Day_3/OpenDoorsInNDoors.java
@@ -0,0 +1,11 @@
+package com.SixPhrase.Day_3;
+
+public class OpenDoorsInNDoors {
+    //1,2,3,...N
+    //2,4,6,...N
+    //3,6,9,...N
+    public static void main(String[] args) {
+        int n = 10;
+        System.out.println((int)Math.sqrt(n));
+    }
+}
